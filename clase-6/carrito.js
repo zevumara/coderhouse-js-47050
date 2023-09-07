89a7592e8965907b26d038ec3685ad3d3f022f19
zevamaru <sebastiancamara@gmail.com>
2023-09-07 20:00:43 -0300

    Mejoramos el carrito.

--- clase-6/carrito.js
+++ clase-6/carrito.js
@@ -1,24 +1,79 @@
 // Variables globales
 const carrito = [];
 
+const arroz = {
+  nombre: "Arroz",
+  precio: 100,
+  cantidad: 1,
+};
+const fideos = {
+  nombre: "Fideos",
+  precio: 200,
+  cantidad: 1,
+};
+const alfajor = {
+  nombre: "Alfajor",
+  precio: 50,
+  cantidad: 1,
+};
+
+carrito.push(arroz);
+carrito.push(fideos);
+carrito.push(alfajor);
+
+// Función que se encargue de buscar si un producto existe en nuestro carrito (array)
+function enCarrito(nombrePrompt) {
+  for (const producto of carrito) {
+    if ((producto.nombre = nombrePrompt)) {
+      return producto;
+    }
+  }
+  return false;
+}
+
 // Función para agregar un producto al carrito
 function agregar() {
   // Pido por prompt los datos del producto
-  const nombre = prompt("Introduzca el nombre del producto:");
-  const precio = prompt("Introduzca el precio del producto:");
+  const nombrePrompt = prompt("Introduzca el nombre del producto:");
+  const precioPrompt = prompt("Introduzca el precio del producto:");
 
   // Creo un objeto con los datos obtenidos del prompt
   const nuevoProducto = {
-    nombre: nombre,
-    precio: parseInt(precio),
-    stock: 1,
+    nombre: nombrePrompt,
+    precio: parseInt(precioPrompt),
+    cantidad: 1,
   };
 
-  // Push agrega el producto en el array
-  carrito.push(nuevoProducto);
+  const productoEncontrado = enCarrito(nombrePrompt);
+
+  if (productoEncontrado) {
+    producto.cantidad++;
+    producto.precio = producto.precio * producto.cantidad;
+  } else {
+    // Push agrega el producto en el array
+    carrito.push(nuevoProducto);
+  }
 
   // Mensaje de alert exitoso
-  alert("El producto " + nombre + " fue agregado al carrito.");
+  alert("El producto " + nombrePrompt + " fue agregado al carrito.");
+  listar();
+
+  // for (const producto of carrito) {
+  //   // Si lo encuentra, solo actualizo la cantidad
+  //   if (producto.nombre == nombrePrompt) {
+  //     producto.cantidad++;
+  //     producto.precio = producto.precio * producto.cantidad;
+  //     // Mensaje de alert exitoso
+  //     alert("El producto " + nombrePrompt + " fue agregado al carrito.");
+  //     listar();
+  //     return;
+  //   }
+  // }
+  // // Push agrega el producto en el array
+  // carrito.push(nuevoProducto);
+  // // Mensaje de alert exitoso
+  // alert("El producto " + nombrePrompt + " fue agregado al carrito.");
+  // listar();
 }
 
 // Función para listar los productos del carrito
@@ -26,31 +81,47 @@ function listar() {
   console.clear();
   console.log("Productos que hay en el carrito:");
 
+  // Recorremos los elementos del array carrito
   for (const producto of carrito) {
-    console.log("-----------------------------------");
+    console.log("----------");
     console.log("Nombre:", producto.nombre);
     console.log("Precio:", producto.precio);
-    console.log("Stock:", producto.stock);
+    console.log("Cantidad:", producto.cantidad);
   }
 }
 
 // Función para quitar un producto del carrito
 function quitar() {
-  const nombrePrompt = prompt("¿Qué producto querés sacar del carrito?");
+  const nombrePrompt = prompt("¿Qué producto querés quitar?");
+
+  const productoEncontrado = enCarrito(nombrePrompt);
+
+  if (productoEncontrado) {
+    const indiceProducto = carrito.indexOf(productoEncontrado);
+    // Una vez obtenemos el índice, lo volamos con splice
+    carrito.splice(indiceProducto, 1);
+    // Mostramos un mensaje al usuario que se ha borrado el producto del carrito
+    alert("El producto " + productoEncontrado.nombre + " fue borrado del carrito.");
+    listar();
+  } else {
+    alert("No se encontró el producto " + nombrePrompt + " en el carrito.");
+  }
 
   // Recorremos nuestro array elemento por elemento, para saber si existe un producto
   // con el nombre indicado en el prompt
-  for (const producto of carrito) {
-    if (producto.nombre == nombrePrompt) {
-      // Encontrado
-      // Necesitamos el índice y lo pedimos con indexOf
-      const indiceProducto = carrito.indexOf(producto);
-      // Una vez obtenemos el índice, lo volamos con splice
-      carrito.splice(indiceProducto, 1);
-      // Mostramos un mensaje al usuario que se ha borrado el producto del carrito
-      alert("El producto " + producto.nombre + " fue borrado del carrito");
-      return;
-    }
-  }
-  alert("No se encontró el producto " + nombrePrompt + " en el carrito");
+  // for (const producto of carrito) {
+  //   if (producto.nombre == nombrePrompt) {
+  //     // Encontrado
+  //     // Necesitamos el índice y lo pedimos con indexOf
+  //     const indiceProducto = carrito.indexOf(producto);
+  //     // Una vez obtenemos el índice, lo volamos con splice
+  //     carrito.splice(indiceProducto, 1);
+  //     // Mostramos un mensaje al usuario que se ha borrado el producto del carrito
+  //     alert("El producto " + producto.nombre + " fue borrado del carrito.");
+  //     return; // El return vacío sale del ciclo y de la función, corta le algoritmo
+  //   }
+  // }
+  // Solo va a llegar a acá si no encontró ningún producto, porque no
+  // entró en el condicional anterior (return)
+  // alert("No se encontró el producto " + nombrePrompt + " en el carrito.");
 }
